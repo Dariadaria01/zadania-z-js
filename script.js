66665ec6bf6bceb419466ee2d0469bfab935b0fa
Dariadaria01 <dariaxdee1@o2.pl>
2024-11-05 19:51:18 +0100

    zadania

---
+++ script.js
@@ -0,0 +1,46 @@
+//Return the Sum of Two Numbers
+//Create a function that takes two numbers as arguments and returns their sum.
+function addition(a, b) {
+  return a + b;
+}
+
+//Convert Minutes into Seconds
+//Write a function that takes an integer minutes and converts it to seconds.
+
+function convert(minutes) {
+	return minutes*60;
+}
+
+//Return the Next Number from the Integer Passed
+//Create a function that takes a number as an argument, increments the number by +1 and returns the result.
+
+function addition(num) {
+	return num + 1;
+}
+
+//Area of a Triangle
+//Write a function that takes the base and height of a triangle and return its area.
+
+function triArea(base, height) {
+	return (base * height/2)
+}
+
+//Convert Age to Days
+//Create a function that takes the age in years and returns the age in days.
+
+function calcAge(age) {
+	return (age * 365);
+}
+
+//Return the First Element in an Array
+//Create a function that takes an array containing only numbers and return the first element.
+
+function getFirstValue(arr) {
+	return arr[0];
+}
+//Convert Hours into Seconds
+//Write a function that converts hours into seconds.
+
+function howManySeconds(hours) {
+	return (hours * 3600);
+}
